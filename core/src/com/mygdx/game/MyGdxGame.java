c5044aa21735293b9339cb2d75b93e6512ffa879
Steve Jobs <peter@steves-mbp.nvstu.edu>
2017-02-22 19:54:03 -0500

    character image adjusts to direction of travel

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -11,9 +11,10 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	float x, y;
+	float x, y, zv;
+	char directionality;
 	TextureRegion upish, downish, rightish, leftish;
-	boolean faceRight, faceLeft;
+	boolean faceRight = true;
 
     static final int TILEWIDTH = 16;
     static final int TILEHEIGHT = 16;
@@ -23,71 +24,85 @@ public class MyGdxGame extends ApplicationAdapter {
 
     static final int DRAW_WIDTH = WIDTH*5;
     static final int DRAW_HEIGHT = HEIGHT*5;
+
+    static final float MAX_VELOCITY = 500;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg"); //tried to change badlogic.jpg -> tiles.png
 
 		Texture tiles = new Texture("tiles.png");
 		TextureRegion[][] grid = TextureRegion.split(tiles, TILEWIDTH, TILEHEIGHT);
 		downish = grid[6][0];
         upish = grid[6][1];
-        rightish = grid[6][2];
-        leftish = grid[6][3];
+        rightish = grid[6][3];
 		TextureRegion right = grid[6][3];
-		TextureRegion left = new TextureRegion(right);
-		left.flip(true, false);
+        leftish = new TextureRegion(right);
+		leftish.flip(true, false);
 	}
 
 	@Override
 	public void render () {
 	    move();
-        //x++; these two just increment the image indefinitely
-        //y++;
 
         TextureRegion img;
-        //use a while loop in place of 'y > 0', so that the character faces the screen UNLESS moving upward
-        if (y > 0) {
-            img = upish;
-        }
-        else {
-            img = downish;
+
+        switch (directionality) {
+
+            case 'u':
+                img = upish;
+                break;
+            case 'd':
+                img = downish;
+                break;
+            case 'r':
+                img = rightish;
+                break;
+            case 'l':
+                img = leftish;
+                break;
+            default:
+                img = downish;
+                break;
         }
 
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		//attempting to change character image to appropriate facing direction
-		if (faceRight) {
-		    batch.draw(img,x,y,DRAW_WIDTH,DRAW_HEIGHT);
+        if (faceRight) {
+            batch.draw(img, x, y, DRAW_WIDTH, DRAW_HEIGHT);
         }
         else {
-		    batch.draw(img,x + DRAW_WIDTH,y,DRAW_WIDTH*-1,DRAW_HEIGHT);
+            batch.draw(img, x + DRAW_WIDTH, y, DRAW_WIDTH * -1, DRAW_HEIGHT);
         }
-
-		//batch.draw(img, x, y); // x, = start location
-		batch.end();
+        batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 
 	//gets character to move about the screen
 	void move() {
-        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+        if(Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
+            //rate = *10;
+        }
+	    if (Gdx.input.isKeyPressed(Input.Keys.UP) /*&& Gdx.input.isKeyPressed(Input.Keys.SPACE)*/) {
             y++;
+            directionality = 'u';
         }
         if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
-            y--;
+             y--;
+            directionality = 'd';
         }
         if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
             x++;
+            directionality = 'r';
         }
-        if(Gdx.input.isKeyPressed(Input.Keys.LEFT))
+        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
             x--;
+            directionality = 'l';
+        }
     }
 }
